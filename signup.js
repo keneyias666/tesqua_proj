5ec6ac818d14a38be31ed6b81156bde60051557d
cadness33 <cadness333@gmail.com>
2025-03-20 19:16:42 +0800

    Update signup.js

--- signup.js
+++ signup.js
@@ -7,9 +7,15 @@ document.getElementById("signup-form").addEventListener("submit", function (even
         firstname: document.getElementById("firstname").value,
         lastname: document.getElementById("lastname").value,
         gender: document.getElementById("gender").value,
-        password: document.getElementById("password").value
+        password: document.getElementById("password").value,
+        confirmPassword: document.getElementById("confirm-password").value
     };
 
+    // Check if passwords match
+    if (userData.password !== userData.confirmPassword) {
+        alert('Passwords do not match!');
+        return; // Prevent further execution and stop form submission
+    }
     fetch("http://localhost:3000/save-user", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
